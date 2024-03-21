db79960aa45f59811716ec2a70d4a07cb9ad8010
Kovalenko, Tim <tima.mtl@gmail.com>
2024-03-21 10:03:34 -0400

    added eza

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,23 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/amro.omp.json" | Invoke-Expression
-If (-Not (Test-Path Variable:PSise)) {  # Only run this in the console and not in the ISE
-    Import-Module Get-ChildItemColor
-    
-    Set-Alias l Get-ChildItemColor -option AllScope
-    Set-Alias ls Get-ChildItemColorFormatWide -option AllScope 
-}
-Set-PSReadLineOption -Colors @{ InlinePrediction = "`e[38;2;240;98;146m" }
-Set-PSReadLineKeyHandler -Chord "RightArrow" -Function ForwardWord
+Set-PSReadLineOption -Colors @{ InlinePrediction = 'Green' }
+
+# If (-Not (Test-Path Variable:PSise)) {  # Only run this in the console and not in the ISE
+#     Import-Module Get-ChildItemColor
+#     
+#     Set-Alias l Get-ChildItemColor -option AllScope
+#     Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
+# }
 Import-Module -Name CompletionPredictor
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -Colors @{ InlinePrediction = "`e[38;2;240;98;146m" }
+Set-PSReadLineKeyHandler -Chord "RightArrow" -Function ForwardWord
 Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
+Set-Alias -Name l -Value Eza-l -option AllScope
+Set-Alias -Name ls -Value Eza-ls -option AllScope
+
+function Eza-l() {
+    eza -l --git --grid --time-style relative
+}
+function Eza-ls() {
+    eza -l --git --time-style relative
+}
