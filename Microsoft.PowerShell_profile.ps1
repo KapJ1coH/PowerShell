259d56c7720145a072d0ab7cb44e2712d71f85c3
KapJ1coH <tima.mtl@gmail.com>
2024-05-08 15:37:16 -0400

    highly improved the loading speed

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,7 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/amro.omp.json" | Invoke-Expression
-Set-PSReadLineOption -Colors @{ InlinePrediction = 'Green' }
+$null = Register-EngineEvent -SourceIdentifier 'PowerShell.OnIdle' -MaxTriggerCount 1 -Action {
+
+    Set-PSReadLineOption -Colors @{ InlinePrediction = 'Green' }
 
 # If (-Not (Test-Path Variable:PSise)) {  # Only run this in the console and not in the ISE
 #     Import-Module Get-ChildItemColor
@@ -7,22 +9,25 @@ Set-PSReadLineOption -Colors @{ InlinePrediction = 'Green' }
 #     Set-Alias l Get-ChildItemColor -option AllScope
 #     Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
 # }
-Import-Module -Name CompletionPredictor
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-Set-PSReadLineOption -Colors @{ InlinePrediction = "`e[38;2;240;98;146m" }
-Set-PSReadLineKeyHandler -Chord "RightArrow" -Function ForwardWord
-Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
-Set-Alias -Name l -Value Eza-l -option AllScope
-Set-Alias -Name ls -Value Eza-ls -option AllScope
-Set-Alias -Name cat -Value bat
+#
+    Import-Module -Name CompletionPredictor
+    Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+    Set-PSReadLineOption -Colors @{ InlinePrediction = "`e[38;2;240;98;146m" }
+    Set-PSReadLineKeyHandler -Chord "RightArrow" -Function ForwardWord
+    Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
+    Set-Alias -Name l -Value Eza-l -option AllScope
+    Set-Alias -Name ls -Value Eza-ls -option AllScope
+    Set-Alias -Name cat -Value bat
 
-function Eza-l() {
-    eza -l --git --grid --time-style relative
-}
-function Eza-ls() {
-    eza -l --git --time-style relative
-}
+    function Eza-l() {
+        eza -l --git --grid --time-style relative
+    }
+    function Eza-ls() {
+        eza -l --git --time-style relative
+    }
 
 
 # For the taskList -> pip install pls-cli
-pls
+    pls
+
+}
